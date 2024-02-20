85cd1b52ba886284fa170fbc2845cede7f501f0f
Dikoume Eboule Charles Darryl <142431183+ddarryl@users.noreply.github.com>
2024-02-20 10:13:20 +0100

    Add files via upload

---
+++ corbeille.js
@@ -0,0 +1,11 @@
+function passageNuit() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.getElementById('image').src = 'Images Corbeille-20180304/Images/Images/Nuit.png';
+}
+
+function retourNormal() {
+    document.body.style.backgroundColor = '';
+    document.body.style.color = '';
+    document.getElementById('image').src = 'Images Corbeille-20180304/Images/Images/Jour.png';
+}
